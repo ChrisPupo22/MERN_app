6bdc32d6790b77d4c72fa9c87cc35030faf69999
ChrisPupo22 <cpupo822@gmail.com>
2020-06-03 18:25:34 -0400

    unique id

---
+++ project/main_project_backend/controllers/places-controller.js
@@ -0,0 +1,74 @@
+const HttpError = require('../models/http-error');
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
+    },
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1'
+  }
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    throw new HttpError('Could not find a place for the provided id.', 404);
+  }
+
+  res.json({ place }); // => { place } => { place: place }
+};
+
+// function getPlaceById() { ... }
+// const getPlaceById = function() { ... }
+
+
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find(p => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    return next(
+      new HttpError('Could not find a place for the provided user id.', 404)
+    );
+  }
+
+  res.json({ place });
+};
+
+const uuid = require('uuid/dist/v4')
+
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  // const title = req.body.title;
+  const createdPlace = {
+    id: Math.floor(Math.random() * 10000), 
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator
+  };
+
+  DUMMY_PLACES.push(createdPlace); //unshift(createdPlace)
+
+  res.status(201).json({place: createdPlace});
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
+
