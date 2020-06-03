0aa0b9ecb1eeddf7832115fc26c91891e40113b1
ChrisPupo22 <cpupo822@gmail.com>
2020-06-03 15:39:33 -0400

    working on back-end

--- project/main_project_backend/routes/places-routes.js
+++ project/main_project_backend/routes/places-routes.js
@@ -1,10 +1,37 @@
 const express = require('express'); 
 
+
 const router = express.Router(); 
 
-router.get('/', (req, res, next) => {
-    console.log("get request in places"); 
-    res.json({message: "it works"}); 
+const DUMMY_PLACES = [
+    {
+        id: 'p1', 
+        title: 'Empire State Building', 
+        description: 'one of the most famous skyscrapers in the world',
+        location: {
+            lat: 40.7484474, 
+            lng: -73.9871516
+        }, 
+        address: '20 W 34th St, New York, NY 10001', 
+        creator: 'u1'
+    }
+]
+
+router.get('/:pid', (req, res, next) => {
+    const placeId = req.params.pid; 
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placeId; 
+
+    })
+    res.json({ place }); 
 });
+
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid; 
+    const place = DUMMY_PLACES.find(u => {
+        return u.creator === userId
+    })
+    res.json({ place })
+})
 
 module.exports = router; 
---
+++ project/main_project_backend/routes/users-routes.js
@@ -0,0 +1,23 @@
+const express = require('express'); 
+
+const places = require('./places-routes')
+
+// const router = express.Router(); 
+
+// const DUMMY_USERS = [
+//     {
+//         id: 'u1',
+//         name: 'Chris Pupo'
+//     }
+// ]
+
+// router.get('/:uid', (req, res, next) => {
+//     const userId = req.params.uid; 
+//     const user = DUMMY_USERS.find(u => {
+//         return u.id === userId
+//     })
+//     res.json({user})
+    
+// })
+
+// module.exports = router; 
